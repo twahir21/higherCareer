87cff5b1f1798916d2a67a0799579f11f6b4c77e
Twahir <twahirsudy3@gmail.com>
2024-12-10 17:51:39 +0300

    latest commit

---
+++ router/public.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const publicRouter = express.Router();
+
+
+publicRouter.get("/parent-register", (req, res) => {
+    res.render('auth/parent-register')
+});
+
+publicRouter.get("/teacher-register", (req, res) => {
+    res.render('auth/teacher-register')
+});
+
+publicRouter.get("/faq", (req, res) => {
+    res.render("layout/faq")
+})
+
+publicRouter.get("/vision", (req, res) => {
+    res.render("layout/vision")
+});
+
+publicRouter.get('/', (req, res) => {
+    res.render("index");
+});
+
+publicRouter.use((req, res) => {
+    res.render("errors/404");
+});
+
+module.exports = publicRouter;
--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.set('views', path.join(__dirname, 'views'));
 // Route to use public files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routers imports
+const publicRouter = require("./router/public");
+
+
+// Router usage
+// keep public router the last
+app.use(publicRouter);
+
 // File download route
 app.get('/download/joining-primary', (req, res) => {
     const file = path.join(__dirname, 'downloads', 'joining-Primary.doc');
@@ -21,33 +29,8 @@ app.get('/download/joining-primary', (req, res) => {
   });
   
 
-app.get("/faq", (req, res) => {
-    res.render("layout/faq")
-})
-
 app.get("/login", (req, res) => {
     res.render('auth/login')
-});
-
-app.get("/parent-register", (req, res) => {
-    res.render('auth/parent-register')
-});
-
-app.get("/teacher-register", (req, res) => {
-    res.render('auth/teacher-register')
-});
-
-// Route to render the 'main' view
-app.get('/', (req, res) => {
-    res.render("index");
-});
-
-app.get("/vision", (req, res) => {
-    res.render("layout/vision")
-})
-
-app.use((req, res) => {
-    res.render("errors/404")
 });
 
 // Start the server
